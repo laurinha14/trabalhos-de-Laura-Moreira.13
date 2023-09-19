4af68a7b11ab42d7166bc010211487c65ce91b4d
laura6778 <144158715+laura6778@users.noreply.github.com>
2023-09-19 11:13:21 -0300

    atividade v1
    logica de programacao

---
+++ ex001.py
@@ -0,0 +1,12 @@
+s = input('digite alguma coisa:')
+print('tem espaco?',s.isspace())
+print('e numerico?',s.isnumeric())
+print('e alfabetico?',s.isalpha())
+print('e alfanumerico?',s.isalnum())
+print('e maiuscula?',s.isupper())
+print('e minuscula?',s.islower())                                                          
+
+n1 = input('digite o primeiro numero')
+n2 = input('digite o segundo numero')
+r = n1+n2
+print('o resultado e:',r)
