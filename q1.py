982b0f425f140c3eabfb8ea305e6284152ceb4b7
laurinha14 <145461191+laurinha14@users.noreply.github.com>
2023-10-10 09:54:21 -0300

    trabalho1

---
+++ q1.py
@@ -0,0 +1,4 @@
+numero = int(input('digite um valor ai mano'))
+s1 = numero + 1
+a = numero -1
+print("analisando o numero {}, o sucessor e{} e o antecessor e{}".format(numero, s1, a))
---
+++ q2.py
@@ -0,0 +1,4 @@
+n1=int(input("digite um numero:"))
+n2=n1*2
+n3=n1*3
+print("o dobro é {}, e o triplo e {}".format(n1, n2, n3))
