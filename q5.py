a19978f563ca8aa4d4e12a9cb7855a90d8a1b248
laurinha14 <145461191+laurinha14@users.noreply.github.com>
2023-10-10 11:13:12 -0300

    trabalho1

---
+++ q5.py
@@ -0,0 +1,3 @@
+d=float(input('digite quanto voce tem na carteira'))
+r=d/5.15
+print=('voce conseque comprar quantos dolares {}',format ,{nt})
---
+++ q6.py
@@ -0,0 +1,6 @@
+altura= float(input("digite a altura"))
+largura=float(input("digite a largura"))
+área=altura*largura
+print('a area da sua parede e:',área,'m²')
+tinta=área/2
+print('a tinta necessaria e:',tinta,'litros')
